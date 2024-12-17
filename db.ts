39f9b4c53d57b12da2885c62e64b26fb0e8734b3
myommt <zai.myo.mintun@gmail.com>
2024-12-17 12:32:54 -0600

    install express

--- db.ts
+++ db.ts
@@ -4,12 +4,15 @@ import mongoose from "mongoose";
 //dotenv.config();
 //const url = process.env.DB_URL
 
-export function connect_db(){
-    if(process.env.DB_URL){
+export function connect_db() {
+    if (process.env.DB_URL) {
         mongoose.connect(process.env.DB_URL)
-            .then(_=>console.log(`connected to local db`))
-            .catch(e=>console.log(`failed to connect to DB`, e));
-        }
+            .then(_ => console.log(`connected to local db`))
+            .catch(e => console.log(`failed to connect to DB`, e));
+    }
+    else {
+        console.log(`no DB URL`);
+    }
 }
 
 
